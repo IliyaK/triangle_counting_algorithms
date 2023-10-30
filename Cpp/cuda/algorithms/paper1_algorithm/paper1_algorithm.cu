324399047bf1a086fbd45d8453a9f66272e4e900
Iliya Kulbaka <illiya.kulbaka@gmail.com>
2023-10-30 17:44:47 -0400

    Organized GPU and CPU based algorithm1 files, and cleaned up the .cu and .h files.

--- Cpp/cuda/algorithms/paper1_algorithm/paper1_algorithm.cu
+++ Cpp/cuda/algorithms/paper1_algorithm/paper1_algorithm.cu
@@ -1,76 +1,255 @@
 #include "../../preprocessing.h"  // to get CSCMatrix struct
 
-__global__ void countEdgesInRowKernel(const int* colPtr, int numVertices, int* result) {
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
+// CUDA
+__global__ void copyUpperLower(int* mat, int* upper, int* lower, int numVertices_edgeList) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (row < numVertices) {
-        result[row] = colPtr[row + 1] - colPtr[row];
+    if (i < numVertices_edgeList && j < numVertices_edgeList) {
+        if (i <= j) {
+            upper[i * numVertices_edgeList + j] = mat[i * numVertices_edgeList + j];
+            lower[j * numVertices_edgeList + i] = mat[i * numVertices_edgeList + j];
+        }
     }
 }
+__global__ void matrixMultiplication(int* product, int* upper, int* lower, int numVertices_edgeList) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-void countEdgesInRowsCUDA(const CSCMatrix& CSC, int numVertices, int* result) {
-    int* d_colPtr;
-    int* d_result;
+    int sum = 0;
+    for (int k = 0; k < numVertices_edgeList; ++k) {
+        sum += upper[i * numVertices_edgeList + k] * lower[k * numVertices_edgeList + j];
+    }
+    product[i * numVertices_edgeList + j] = sum;
+}
+__global__ void matrixElementWiseMultiply(int* mat, int* product, int* result, int numVertices_edgeList) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    // Allocate memory on the device
-    cudaMalloc((void**)&d_colPtr, (CSC.colPtr.size()) * sizeof(int));
-    cudaMalloc((void**)&d_result, numVertices * sizeof(int));
+    if (i < numVertices_edgeList && j < numVertices_edgeList) {
+        result[i * numVertices_edgeList + j] = mat[i * numVertices_edgeList + j] * product[i * numVertices_edgeList + j];
+    }
+}
+__global__ void sumResultMatrix(int* result, int* sum, int numVertices_edgeList) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (i < numVertices_edgeList && j < numVertices_edgeList) {
+        atomicAdd(sum, result[i * numVertices_edgeList + j]);
+    }
+}
+///////////////
+
+
+int algorithm1_gpu(const std::string& filename){
+    std::vector<std::pair<int, int>> edgeList = edgeLine_parser(filename);
+
+    int numVertices_edgeList = getNumberOfVertices(edgeList);
+
+
+    int sum = 0;
+
+    int size = numVertices_edgeList * numVertices_edgeList * sizeof(int);
+    std::size_t arr_size = numVertices_edgeList * numVertices_edgeList;
+    int *mat = new int[arr_size]();
+
+    // making adjacency matrix
+    for (const auto &edge : edgeList) {
+        int vertex1 = edge.first;
+        int vertex2 = edge.second;
+
+        // Set the elements to 1 to indicate the presence of an edge
+        mat[vertex1 * numVertices_edgeList + vertex2] = 1;
+        mat[vertex2 * numVertices_edgeList + vertex1] = 1;
+    }
+    std::cout << "graph parsed" << std::endl;
+
+    int *upper = new int[arr_size]();
+    int *lower = new int[arr_size]();
+
+    // splitting into upper and lower
+
+    // Allocate device memory for mat, upper, and lower
+    int* d_mat;
+    int* d_upper;
+    int* d_lower;
+
+    cudaMalloc((void**)&d_mat, size);
+    cudaMalloc((void**)&d_upper, size);
+    cudaMalloc((void**)&d_lower, size);
 
     // Copy data from host to device
-    cudaMemcpy(d_colPtr, &CSC.colPtr[0], (CSC.colPtr.size()) * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mat, mat, size, cudaMemcpyHostToDevice);
 
-    // Define the block size and grid size
-    int blockSize = 256;
-    int gridSize = (numVertices + blockSize - 1) / blockSize;
+    // Define grid and block dimensions for the CUDA kernel
+    dim3 dimGrid4((numVertices_edgeList + 15) / 16, (numVertices_edgeList + 15) / 16);
+    dim3 dimBlock4(16, 16);
+
+    // Launch the CUDA kernel to copy upper and lower triangular elements
+    copyUpperLower<<<dimGrid4, dimBlock4>>>(d_mat, d_upper, d_lower, numVertices_edgeList);
+
+    // Copy the results back from device to host if needed
+    cudaMemcpy(upper, d_upper, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(lower, d_lower, size, cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(d_mat);
+    cudaFree(d_upper);
+    cudaFree(d_lower);
+
+
+    int *product = new int[arr_size]();
+
+
+    // multiply upper and lower
+    int *d_product;
+
+    // Allocate memory on the GPU
+    cudaMalloc((void**)&d_product, size);
+    cudaMalloc((void**)&d_upper, size);
+    cudaMalloc((void**)&d_lower, size);
+
+    // Copy data from host to device
+    cudaMemcpy(d_upper, upper, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_lower, lower, size, cudaMemcpyHostToDevice);
+
+    // Define grid and block dimensions
+    dim3 dimGrid(numVertices_edgeList, numVertices_edgeList);
+    dim3 dimBlock(1, 1); // Adjust the block size as needed
 
     // Launch the CUDA kernel
-    countEdgesInRowKernel<<<gridSize, blockSize>>>(d_colPtr, numVertices, d_result);
+    matrixMultiplication<<<dimGrid, dimBlock>>>(d_product, d_upper, d_lower, numVertices_edgeList);
 
     // Copy the result back to the host
-    cudaMemcpy(result, d_result, numVertices * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(product, d_product, size, cudaMemcpyDeviceToHost);
 
-    // Free allocated memory on the device
-    cudaFree(d_colPtr);
-    cudaFree(d_result);
-}
+    // Free GPU memory
+    cudaFree(d_product);
+    cudaFree(d_upper);
+    cudaFree(d_lower);
+    // multiplying upper and lower
 
-__global__ void nzindices(int *dest, const int *u, int num_rows, int num_cols)
-{
-    const int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < num_rows) {
-        if (u[i] != 0) {
-            dest[i] = 1;
-        }
-        else{
-            dest[i] = 0;
-        }
-    }
-}
-void NonZeroIndices(int numVertices, int* uRowSum, int* uNonZero) {
-    int* d_uRowSum;
-    int* d_uNonZero;
+    delete[] upper;
+    delete[] lower;
 
-    // Allocate memory on the device
-    cudaMalloc((void**)&d_uRowSum, numVertices * sizeof(int));
-    cudaMalloc((void**)&d_uNonZero, numVertices * sizeof(int));
+    int *result = new int[arr_size]();
+
+    int* d_result;
+
+    cudaMalloc((void**)&d_mat, size);
+    cudaMalloc((void**)&d_product, size);
+    cudaMalloc((void**)&d_result, size);
 
     // Copy data from host to device
-    cudaMemcpy(d_uRowSum, uRowSum, numVertices * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mat, mat, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_product, product, size, cudaMemcpyHostToDevice);
 
-    // Define the block size and grid size for countEdgesInRowKernel
-    int blockSize = 256;
-    int gridSize = (numVertices + blockSize - 1) / blockSize;
+    // Define grid and block dimensions for the CUDA kernel
+    dim3 dimGrid2((numVertices_edgeList + 15) / 16, (numVertices_edgeList + 15) / 16);
+    dim3 dimBlock2(16, 16);
 
-    // Define the block size and grid size for nzindices
-    gridSize = (numVertices + blockSize - 1) / blockSize;
+    // Launch the CUDA kernel
+    matrixElementWiseMultiply<<<dimGrid2, dimBlock2>>>(d_mat, d_product, d_result, numVertices_edgeList);
 
-    // Launch the nzindices kernel
-    nzindices<<<gridSize, blockSize>>>(d_uNonZero, d_uRowSum, numVertices, numVertices);
+    // Copy the result back from device to host
+    cudaMemcpy(result, d_result, size, cudaMemcpyDeviceToHost);
 
-    // Copy the result back to the host
-    cudaMemcpy(uNonZero, d_uNonZero, numVertices * sizeof(int), cudaMemcpyDeviceToHost);
+    // Free device memory
+    cudaFree(d_mat);
+    cudaFree(d_product);
+    cudaFree(d_result);
 
-    // Free allocated memory on the device
-    cudaFree(d_uRowSum);
-    cudaFree(d_uNonZero);
+    // element wise multiplication of mat and product
+
+
+    delete[] mat;
+    delete[] product;
+
+
+    // summing up the matrix
+
+
+    // Allocate device memory for result and sum
+    int* d_sum;
+
+    cudaMalloc((void**)&d_result, size);
+    cudaMalloc((void**)&d_sum, sizeof(int));
+
+    // Copy data from host to device
+    cudaMemcpy(d_result, result, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_sum, &sum, sizeof(int), cudaMemcpyHostToDevice);
+
+    // Define grid and block dimensions for the CUDA kernel
+    dim3 dimGrid3((numVertices_edgeList + 15) / 16, (numVertices_edgeList + 15) / 16);
+    dim3 dimBlock3(16, 16);
+
+    // Launch the CUDA kernel to compute the sum
+    sumResultMatrix<<<dimGrid3, dimBlock3>>>(d_result, d_sum, numVertices_edgeList);
+
+    // Copy the sum back from device to host
+    cudaMemcpy(&sum, d_sum, sizeof(int), cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(d_result);
+    cudaFree(d_sum);
+
+    delete[] result;
+
+    return sum;
+}
+
+int algorithm1_cpu(const std::string& filename){
+    std::vector<std::pair<int, int>> edgeList = edgeLine_parser(filename);
+
+    int numVertices_edgeList = getNumberOfVertices(edgeList);
+
+    int sum = 0;
+
+    int size = numVertices_edgeList * numVertices_edgeList * sizeof(int);
+    std::size_t arr_size = numVertices_edgeList * numVertices_edgeList;
+    int *mat = new int[arr_size]();
+    int *upper = new int[arr_size]();
+    int *lower = new int[arr_size]();
+    int *product = new int[arr_size]();
+    int *result = new int[arr_size]();
+
+
+    for (int i = 0; i < numVertices_edgeList; i++) {
+        for (int j = 0; j < numVertices_edgeList; j++) {
+            if (i <= j) {
+                upper[i * numVertices_edgeList + j] = mat[i * numVertices_edgeList + j];
+                lower[j * numVertices_edgeList + i] = mat[i * numVertices_edgeList + j];
+            }
+        }
+    }
+
+    for (int i = 0; i < numVertices_edgeList; ++i) {
+        for (int j = 0; j < numVertices_edgeList; ++j) {
+            for (int k = 0; k < numVertices_edgeList; ++k) {
+                product[i * numVertices_edgeList + j] += upper[i * numVertices_edgeList + k] * lower[k * numVertices_edgeList + j];
+            }
+        }
+    }
+
+    delete[] upper;
+    delete[] lower;
+
+    for (int i = 0; i < numVertices_edgeList; i++) {
+        for (int j = 0; j < numVertices_edgeList; j++) {
+            // Calculate the element-wise product and store it in the result matrix
+            result[i * numVertices_edgeList + j] = mat[i * numVertices_edgeList + j] * product[i * numVertices_edgeList + j];
+        }
+    }
+
+    for (int i = 0; i < numVertices_edgeList; i++) {
+        for (int j = 0; j < numVertices_edgeList; j++) {
+            sum += result[i * numVertices_edgeList + j];
+        }
+    }
+
+    delete[] result;
+    delete[] product;
+    delete[] mat;
+
+    return sum;
 }
--- Cpp/cuda/algorithms/paper1_algorithm/paper1_algorithm.h
+++ Cpp/cuda/algorithms/paper1_algorithm/paper1_algorithm.h
@@ -1,10 +1,11 @@
 #ifndef CPP_PAPER1_ALGORITHM_H
 #define CPP_PAPER1_ALGORITHM_H
 
-void countEdgesInRowsCUDA(const CSCMatrix& CSC, int numVertices, int* result);
-__global__ void countEdgesInRowKernel(const int* colPtr, int numVertices, int* result);
+__global__ void copyUpperLower(int* mat, int* upper, int* lower, int numVertices_edgeList);
+__global__ void matrixMultiplication(int* product, int* upper, int* lower, int numVertices_edgeList);
+__global__ void matrixElementWiseMultiply(int* mat, int* product, int* result, int numVertices_edgeList);
+__global__ void sumResultMatrix(int* result, int* sum, int numVertices_edgeList);
 
-void NonZeroIndices(int numVertices, int* uRowSum, int* uNonZero);
-__global__ void nzindices(int *dest, const int *u, int num_rows, int num_cols);
-
+int algorithm1_gpu(const std::string& filename);
+int algorithm1_cpu(const std::string& filename);
 #endif //CPP_PAPER1_ALGORITHM_H
